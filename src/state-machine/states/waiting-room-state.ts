0e202f4dbc8304af1eb3c0ca3a6ce6e3d84e0773
Vinicius P Stormoski <vinicius.stormoski@conectanuvem.com.br>
2025-08-20 15:56:36 -0300

    fix: fixed bot image video in meeting

--- src/state-machine/states/waiting-room-state.ts
+++ src/state-machine/states/waiting-room-state.ts
@@ -3,7 +3,6 @@ import { ScreenRecorderManager } from '../../recording/ScreenRecorder'
 import { GLOBAL } from '../../singleton'
 import { Streaming } from '../../streaming'
 import { HtmlSnapshotService } from '../../services/html-snapshot-service'
-import { playBranding } from '../../branding'
 
 import {
     MeetingEndReason,
@@ -176,10 +175,8 @@ export class WaitingRoomState extends BaseState {
                     () => {
                         joinSuccessful = true
                         console.log('Join successful notification received')
-
-                        // Activate branding after camera is activated
                         if (GLOBAL.get().custom_branding_bot_path) {
-                            playBranding()
+                            console.log('Custom branding is active via browser fake video capture')
                         }
                     },
                 )
